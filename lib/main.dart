9f1df8598ee4b837f9438742ee1aa316c184e076
Anand jha <codewithann@gmail.com>
2024-02-29 08:59:24 +0530

    created instance of shared preference
        - TODO: Now i am going to work on the ui part of FitFLow then switch on
                shared preference.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:fitflow/app.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
+SharedPreferences? prefs;
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+// instances of var prefs.
+  prefs = await SharedPreferences.getInstance();
+
   runApp(const MyApp());
 }
-
-
